2dd2e9caea153e07830bae18a86a95528492138c
CozyLife <takeshiokada@okatakese.com>
2023-04-16 23:31:55 +0900

    編集ボタン、削除ボタンを追加（押したときの挙動はまだ）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,10 @@ import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
 import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { map } from 'rxjs/operators'
+import { AngularFireDatabase, AngularFireList, SnapshotAction } from '@angular/fire/compat/database';
 
 const CURRENT_USER: User = new User(1, '岡田 孟');
-const ANOTHER_USER: User = new User(2, '武井 健司');
-
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れ様です。'),
-  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
-  new Comment(CURRENT_USER, 'お疲れ様です。'),
-  new Comment(CURRENT_USER, 'クライアントからOKがでました')
-];
 
 @Component({
   selector: 'ac-root',
@@ -22,18 +15,30 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
 
-  comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = '';
   item$: Observable<any>;
 
   constructor(private db: AngularFireDatabase) {
     this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.snapshotChanges()
+      .pipe(
+        map((snapshots: SnapshotAction<Comment>[]) => {
+          return snapshots.map(snapshot => {
+            const value = snapshot.payload.val();
+            return new Comment({key: snapshot.payload.key, ...value});
+          });
+        })
+      );
   }
 
   addComment(comment: string): void {
     if(comment) {
-      this.comments.push(new Comment(this.currentUser, comment))
+      this.commentsRef.push(new Comment({ user: this.currentUser, message: comment}))
+      this.comment = '';
     }
   }
 }
